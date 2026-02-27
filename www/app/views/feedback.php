44a8cca2ac3e8a7f4bcba336fe69d72e1170d829
FJDijkstra <167628132+FJDijkstra@users.noreply.github.com>
2026-02-27 13:28:38 +0100

    lege feedback

--- www/app/views/feedback.php
+++ www/app/views/feedback.php
@@ -10,6 +10,9 @@
     <h2 class="text-center">Huidige Feedback</h2>
     <div class="mx-auto w-75 h-50 bg-white border rounded-2 container text-center p-2 overflow-auto">
     <?php
+        if ($feedback == []) {
+            echo "<p class='text-muted'>De website is perfect, er is geen feedback</p>";
+        }
         foreach ($feedback as $key => $fb) {
             echo "<div class='m-1 row'><span class='col'>$fb->feedback</span>";
             echo "<span class='col-4 float-end text-muted small'>$fb->timestamp</span></div>";
