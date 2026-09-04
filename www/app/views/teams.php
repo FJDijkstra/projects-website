fac7350d5386b7ce4cc661cfe42ea63592a399d2
FJDijkstra <167628132+FJDijkstra@users.noreply.github.com>
2026-09-04 15:35:51 +0200

    fixed scroll issue

--- www/app/views/teams.php
+++ www/app/views/teams.php
@@ -1,4 +1,4 @@
-<div class="h-100 overflow-y-auto">
+<div style="height: 90%"class="overflow-y-auto">
 <div class="text-center m-4 mb-5 pb-5">
 <h3>Kies een sessie</h3>
 <form action="/buzzer/teams" class="form-group p-4">
@@ -50,6 +50,8 @@
     <label  class=" m-2 my-auto" for="sessionname">Wat is je sessienaam en wachtwoord:</label>
     <div class="d-flex">
         <input type="text" id="sessionname" name="sessionname" placeholder="Vul hier je sessienaam in..." class='form-control m-2' minlength="1" maxlength="20">
+    </div>
+    <div class="d-flex">
         <input type="text" id="sessionpassword" name="sessionpassword" placeholder="Vul hier je wachtwoord in..." class='form-control m-2' minlength="1" maxlength="20">
         <input class='btn btn-primary m-2' type="submit" value="Toevoegen">
     </div>
